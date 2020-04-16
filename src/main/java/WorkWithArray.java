9f3d3a7137c137122161bcb4ba9c8274a76bd321
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-15 18:15:09 -0700

    add unit tests for WorkWithString.frontAgain

---
+++ src/main/java/WorkWithArray.java
@@ -0,0 +1,7 @@
+public class WorkWithArray {
+
+    public boolean firstLast6(int[] nums) {
+        return true;
+
+    }
+}
---
+++ src/test/java/StringsTestSuite.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringsTestSuite {
+    @Test
+    public void edited_should_be_true() {
+        assertEquals(true, WorkWithString.frontAgain("edited"));
+    }
+
+    @Test
+    public void edit_should_be_false() {
+        assertEquals(false, WorkWithString.frontAgain("edit"));
+    }
+
+    @Test
+    public void ed_should_be_true() {
+        assertEquals(true, WorkWithString.frontAgain("ed"));
+    }
+
+    @Test
+    public void jjj_should_be_true() {
+        assertEquals(true, WorkWithString.frontAgain("jjj"));
+    }
+
+    @Test
+    public void empty_string_should_be_false() {
+        assertEquals(false, WorkWithString.frontAgain(""));
+    }
+}
+
