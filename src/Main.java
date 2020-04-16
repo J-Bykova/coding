8bde5029c118c8c6eae89c084ecbc451bed1e353
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-15 17:24:20 -0700

    add frontAgain

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("'redxx' should be 'red': " + WorkWithString.seeColor("redxx"));
+        System.out.println("'edited' should be 'true': " + WorkWithString.frontAgain("edited"));
         System.out.println("----------");
-        System.out.println("'xxred' should be '': " + WorkWithString.seeColor("xxred"));
+        System.out.println("'edit' should be 'false': " + WorkWithString.frontAgain("edit"));
         System.out.println("----------");
-        System.out.println("'blueTimes' should be 'blue': " + WorkWithString.seeColor("blueTimes"));
+        System.out.println("'ed' should be 'true': " + WorkWithString.frontAgain("ed"));
         System.out.println("----------");
-        System.out.println("'' should be '' " + WorkWithString.seeColor(""));
+        System.out.println("'jjj' should be 'true' " + WorkWithString.frontAgain("jjj"));
+        System.out.println("----------");
+        System.out.println("'' should be 'false': " + WorkWithString.frontAgain(""));
         System.out.println("----------");
 
     }
--- src/WorkWithString.java
+++ src/WorkWithString.java
@@ -1,5 +1,24 @@
 public class WorkWithString {
 
+    public static boolean frontAgain(String str) {
+        if (str.isEmpty()) {
+            return false;
+        }
+        if (str.length() == 1) {
+            return false;
+        }
+        if (str.length() == 2) {
+            return true;
+        }
+        String firstLetterOfStr = str.substring(0, 2);
+        String lastLetterOfStr = str.substring(str.length() - 2);
+
+        if (firstLetterOfStr.equals(lastLetterOfStr)) {
+            return true;
+        }
+        return false;
+    }
+
     public static String seeColor(String str) {
         if (str.startsWith("red")) {
             return "red";
