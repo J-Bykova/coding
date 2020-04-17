a3dc7430d4f0b204ada17bd37d92f56b24f9e199
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-17 08:52:37 -0700

    reneme sum3 to sunArr

--- src/test/java/work_with_array/SumArrTestSuite.java
+++ src/test/java/work_with_array/SumArrTestSuite.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class Sum3TestSuite {
+public class SumArrTestSuite {
     @Test
     public void test1() {
         int[] arr = {};
