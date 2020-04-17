4598c1c7a2c824ad7964927c09b188b2e5dd3f75
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-17 08:51:00 -0700

    rename sum3 to sumArr

--- src/main/java/work_with_array/SumArr.java
+++ src/main/java/work_with_array/SumArr.java
@@ -1,7 +1,7 @@
 package work_with_array;
 
-public class Sum3 {
-    static int sum3(int[] nums) {
+public class SumArr {
+    static int sumArr(int[] nums) {
         int result = 0;
         for (int i = 0; i < nums.length; i++) {
             result = result + nums[i];
--- src/test/java/work_with_array/Sum3TestSuite.java
+++ src/test/java/work_with_array/Sum3TestSuite.java
@@ -9,13 +9,13 @@ public class Sum3TestSuite {
     public void test1() {
         int[] arr = {};
         int result = 0;
-        assertEquals(result, Sum3.sum3(arr));
+        assertEquals(result, SumArr.sumArr(arr));
     }
 
     @Test
     public void test2() {
         int[] arr = {1, 2, 3};
         int result = 6;
-        assertEquals(result, Sum3.sum3(arr));
+        assertEquals(result, SumArr.sumArr(arr));
     }
 }
