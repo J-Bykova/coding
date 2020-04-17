1edf50ae1891e5ce3125a1681ac62924142bd311
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-17 09:47:47 -0700

    added method sum2 and added test for method sum2

--- src/test/java/work_with_array/SameFirstLastTestSuite.java
+++ src/test/java/work_with_array/SameFirstLastTestSuite.java
@@ -1,6 +1,6 @@
 package work_with_array;
 
-import org.junit.Assert;
+import org.junit.Assert;;
 import org.junit.Test;
 
 public class SameFirstLastTestSuite {
---
+++ src/test/java/work_with_array/Sum2TestSuite.java
@@ -0,0 +1,29 @@
+package work_with_array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Sum2TestSuite {
+    @Test
+    public void test1() {
+        int[] arr = {};
+        int result = 0;
+        assertEquals(result, Sum2.sum2(arr));
+    }
+
+    @Test
+    public void test2() {
+        int[] arr = {1};
+        int result = 1;
+        assertEquals(result, Sum2.sum2(arr));
+    }
+
+    @Test
+    public void test3() {
+        int[] arr = {1, 2, 3};
+        int result = 3;
+        assertEquals(result, Sum2.sum2(arr));
+    }
+}
