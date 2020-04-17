7799dd6d2bbacb187c3f7660645da8ade944f25f
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-17 16:08:03 -0700

    added method double23 and added tests for method double23

---
+++ src/main/java/work_with_array/Double23.java
@@ -0,0 +1,16 @@
+package work_with_array;
+
+public class Double23 {
+    static boolean double23(int[] nums) {
+        if (nums.length == 0 || nums.length == 1) {
+            return false;
+        }
+        if (nums[0] == 2 && nums[1] == 2) {
+            return true;
+        }
+        if (nums[0] == 3 && nums[1] == 3) {
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/work_with_array/SumArr.java
+++ src/main/java/work_with_array/SumArr.java
@@ -5,6 +5,7 @@ public class SumArr {
         int result = 0;
         for (int i = 0; i < nums.length; i++) {
             result = result + nums[i];
+
         }
         return result;
     }
