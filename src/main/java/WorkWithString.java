0030368567a74a4f7f624c16876de89dd91edba3
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-17 06:24:08 -0700

    add method deFront and add tests for method deFront

--- src/main/java/WorkWithString.java
+++ src/main/java/WorkWithString.java
@@ -1,5 +1,24 @@
 public class WorkWithString {
 
+    public static String deFront(String str) {
+        if (str.isEmpty()) {
+            return str;
+        }
+        if (str.length() == 1 && str.equals("a")) {
+            return str;
+        }
+        if (str.substring(0, 1).equals("a") && str.substring(1, 2).equals("b")) {
+            return str;
+        }
+        if (str.substring(0, 1).equals("a")) {
+            return "a" + str.substring(2);
+        }
+        if (str.substring(1, 2).equals("b")) {
+            return "b" + str.substring(2);
+        }
+        return str.substring(2);
+    }
+
     public static String without2(String str) {
         if (str.isEmpty()) {
             return str;
