57751b0759c4e73452b54946d9aeda0c5f49029f
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-16 18:08:53 -0700

    add method without2 and add tests for method without2

--- src/main/java/WorkWithString.java
+++ src/main/java/WorkWithString.java
@@ -1,5 +1,24 @@
 public class WorkWithString {
 
+    public static String without2(String str) {
+        if (str.isEmpty()) {
+            return str;
+        }
+        if (str.length() == 1) {
+            return str;
+        }
+        if (str.length() == 2) {
+            return "";
+        }
+        if (str.length() == 3 && str.substring(0, 2).equals(str.substring(str.length() - 2))) {
+            return str.substring(0, 1);
+        }
+        if (str.substring(0, 2).equals(str.substring(str.length() - 2))) {
+            return str.substring(2);
+        }
+        return str;
+    }
+
     public static String extraFront(String str) {
         if (str.isEmpty()) {
             return str;
