8ed6f824b1535584f527a50b13cd7019a574e156
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-19 10:54:11 -0700

    add mapAB

---
+++ src/main/java/work_with_map/first_level/MapAB.java
@@ -0,0 +1,12 @@
+package work_with_map.first_level;
+
+import java.util.Map;
+
+public class MapAB {
+    static Map<String, String> mapAB(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            map.put("ab", map.get("a") + map.get("b"));
+        }
+        return map;
+    }
+}
