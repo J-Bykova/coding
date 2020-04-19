d9d85ec390de890a14cd5f4544509f13ae10f583
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-19 10:27:59 -0700

    added method topping1

---
+++ src/main/java/work_with_map/first_level/Topping1.java
@@ -0,0 +1,14 @@
+package work_with_map.first_level;
+
+import java.util.Map;
+
+public class Topping1 {
+    static Map<String, String> topping1(Map<String, String> map) {
+        if (map.containsKey("ice cream")){
+            map.put("ice cream", "cherry");
+        }
+        map.put("bread", "butter");
+        return map;
+    }
+
+}
---
+++ src/test/java/work_with_map/first_level/Topping1TestSuite.java
@@ -0,0 +1,11 @@
+package work_with_map.first_level;
+
+import org.junit.Assert;
+import org.junit.Test;
+import work_with_string.first_level.DeFront;
+
+public class Topping1TestSuite {
+    @Test
+    public void test1() {
+    }
+}
