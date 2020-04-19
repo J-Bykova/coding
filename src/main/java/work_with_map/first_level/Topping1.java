fa5a8d6298b3b6b379330e8be938b396190afd13
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-19 10:39:54 -0700

    add topping2 method

--- src/main/java/work_with_map/first_level/Topping1.java
+++ src/main/java/work_with_map/first_level/Topping1.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class Topping1 {
     static Map<String, String> topping1(Map<String, String> map) {
-        if (map.containsKey("ice cream")){
+        if (map.containsKey("ice cream")) {
             map.put("ice cream", "cherry");
         }
         map.put("bread", "butter");
--- src/main/java/work_with_map/first_level/Topping2.java
+++ src/main/java/work_with_map/first_level/Topping2.java
@@ -2,14 +2,15 @@ package work_with_map.first_level;
 
 import java.util.Map;
 
-public class MapBully {
-    static Map<String, String> mapBully(Map<String, String> map) {
+public class Topping2 {
+    public Map<String, String> topping2(Map<String, String> map) {
         if (map.containsKey("ice cream")) {
-            map.put("yogurt", map.get("ice cream"));
+            map.put("yogurt", "ice cream");
         }
         if (map.containsKey("spinach")) {
             map.put("spinach", "nuts");
         }
         return map;
     }
+
 }
