f6073b546ace89c5bba876be2ae6c986388d6f8b
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-19 10:46:35 -0700

    add method topping3

---
+++ src/main/java/work_with_map/first_level/Topping3.java
@@ -0,0 +1,15 @@
+package work_with_map.first_level;
+
+import java.util.Map;
+
+public class Topping3 {
+    static Map<String, String> topping3(Map<String, String> map) {
+        if (map.containsKey("potato")) {
+            map.put("fries", map.get("potato"));
+        }
+        if (map.containsKey("salad")) {
+            map.put("spinach", map.get("salad"));
+        }
+        return map;
+    }
+}
