4657e5055e02fe7c58ccfc0e1aafeffe7e9b7177
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-20 12:09:25 -0700

    Extract operation to variable

--- src/main/java/work_with_logic/first_level/NearTen.java
+++ src/main/java/work_with_logic/first_level/NearTen.java
@@ -2,9 +2,8 @@ package work_with_logic.first_level;
 
 public class NearTen {
     static boolean nearTen(int num) {
-        if (num % 10 <= 2 || num % 10 >= 8) {
-            return true;
-        }
-        return false;
+        int rest = num % 10;
+
+        return rest <= 2 || rest >= 8;
     }
 }
