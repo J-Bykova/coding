d2375a623e7870c2bc7deb0d2c0b7c092e5aa582
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-21 12:46:33 -0700

    Rename tests in Double23TestsSuite

--- src/test/java/work_with_array/first_level/Double23TestsSuite.java
+++ src/test/java/work_with_array/first_level/Double23TestsSuite.java
@@ -5,32 +5,32 @@ import org.junit.Test;
 
 public class Double23TestsSuite {
     @Test
-    public void test1() {
-        int[] arr = {};
-        Assert.assertFalse(Double23.double23(arr));
+    public void should_return_false_if_got_empty_array() {
+        int[] inputArr = {};
+        Assert.assertFalse(Double23.double23(inputArr));
     }
 
     @Test
-    public void test2() {
-        int[] arr = {1};
-        Assert.assertFalse(Double23.double23(arr));
+    public void should_return_false_if_got_array_with_one_element() {
+        int[] inputArr = {1};
+        Assert.assertFalse(Double23.double23(inputArr));
     }
 
     @Test
-    public void test3() {
-        int[] arr = {2, 2};
-        Assert.assertTrue(Double23.double23(arr));
+    public void should_return_true_if_got_array_contains_2_twice() {
+        int[] inputArr = {2, 2};
+        Assert.assertTrue(Double23.double23(inputArr));
     }
 
     @Test
-    public void test4() {
-        int[] arr = {3, 3};
-        Assert.assertTrue(Double23.double23(arr));
+    public void should_return_true_if_got_array_contains_3_twice() {
+        int[] inputArr = {3, 3};
+        Assert.assertTrue(Double23.double23(inputArr));
     }
 
     @Test
-    public void test5() {
-        int[] arr = {2, 3};
-        Assert.assertFalse(Double23.double23(arr));
+    public void should_return_false_if_got_array_contains_different_elements() {
+        int[] inputArr = {2, 3};
+        Assert.assertFalse(Double23.double23(inputArr));
     }
 }
