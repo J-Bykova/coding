e0484ef7ace214038f4b35419d6b3dedd2d23c18
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-21 17:02:26 -0700

    Renamed tests for SameFirstLastTestSuite class

--- src/test/java/work_with_array/first_level/SameFirstLastTestSuite.java
+++ src/test/java/work_with_array/first_level/SameFirstLastTestSuite.java
@@ -5,31 +5,31 @@ import org.junit.Test;
 
 public class SameFirstLastTestSuite {
     @Test
-    public void test1() {
+    public void should_return_false_if_got_empty_array() {
         int[] arr = {};
         Assert.assertFalse(SameFirstLast.sameFirstLast(arr));
     }
 
     @Test
-    public void test2() {
+    public void should_return_true_if__array_size_is_1() {
         int[] arr = {1};
         Assert.assertTrue(SameFirstLast.sameFirstLast(arr));
     }
 
     @Test
-    public void test3() {
+    public void should_return_true_if_the_first_element_and_the_last_element_are_equal_for_array_size_is_2() {
         int[] arr = {1, 1};
         Assert.assertTrue(SameFirstLast.sameFirstLast(arr));
     }
 
     @Test
-    public void test4() {
+    public void should_return_true_if_the_first_element_and_the_last_element_are_equal_for_array_size_is_4() {
         int[] arr = {1, 2, 3, 1};
         Assert.assertTrue(SameFirstLast.sameFirstLast(arr));
     }
 
     @Test
-    public void test5() {
+    public void should_return_true_if_the_first_element_and_the_last_element_are_equal_for_array_size_is_3() {
         int[] arr = {1, 2, 3};
         Assert.assertFalse(SameFirstLast.sameFirstLast(arr));
     }
