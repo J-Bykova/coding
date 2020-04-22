163ebef5ae36cc44719d2146ff4da739c82bd14c
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-04-21 17:11:14 -0700

    Renamed tests for SumArrTestSuite class

--- src/test/java/work_with_array/first_level/SumArrTestSuite.java
+++ src/test/java/work_with_array/first_level/SumArrTestSuite.java
@@ -6,14 +6,14 @@ import static org.junit.Assert.assertEquals;
 
 public class SumArrTestSuite {
     @Test
-    public void test1() {
+    public void should_return_0_if_got_empty_array() {
         int[] arr = {};
         int result = 0;
         assertEquals(result, SumArr.sumArr(arr));
     }
 
     @Test
-    public void test2() {
+    public void should_return_sum_all_elements_array() {
         int[] arr = {1, 2, 3};
         int result = 6;
         assertEquals(result, SumArr.sumArr(arr));
