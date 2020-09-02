ddaa743d25a12e677642b1f987614ef95b32409b
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-09-02 11:39:34 +0300

    Add MixStartTestSuite

---
+++ src/test/java/work_with_warmup/first_level/MixStartTestSuite.java
@@ -0,0 +1,22 @@
+package work_with_warmup.first_level;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MixStartTestSuite {
+    @Test
+    public void test1(){
+        Assert.assertTrue(MixStart.mixStart("mix snacks"));
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertFalse(MixStart.mixStart("pix snacks"));
+    }
+
+    @Test
+    public void test3(){
+        Assert.assertFalse(MixStart.mixStart("piz snacks"));
+    }
+
+}
