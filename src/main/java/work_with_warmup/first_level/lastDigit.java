ac0fd96e618baa7fb84468a285e9a8cf9383ddd6
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-09-20 10:56:49 +0300

    Add lastDigitTestSuite

--- src/main/java/work_with_warmup/first_level/lastDigit.java
+++ src/main/java/work_with_warmup/first_level/lastDigit.java
@@ -3,7 +3,9 @@ package work_with_warmup.first_level;
 public class lastDigit {
     static final boolean lastDigit(int a, int b) {
 
-        
-        return true;
+        int remainderA = a % 10;
+        int remainderB = b % 10;
+
+        return remainderA == remainderB;
     }
 }
--- src/test/java/work_with_warmup/first_level/lastDigitTestSuite.java
+++ src/test/java/work_with_warmup/first_level/lastDigitTestSuite.java
@@ -1,5 +1,27 @@
 package work_with_warmup.first_level;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class lastDigitTestSuite {
+    @Test
+    public void test() {
+        Assert.assertTrue(lastDigit.lastDigit(7, 17));
+    }
+
+    @Test
+    public void test1() {
+        Assert.assertTrue(lastDigit.lastDigit(27, 17));
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertFalse(lastDigit.lastDigit(2, 17));
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertFalse(lastDigit.lastDigit(42, 17));
+    }
 
 }
